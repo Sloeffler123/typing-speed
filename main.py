c69a7ac6d9927e128513cbada66befd04b3ec994
Sloeffler123 <samloeffler123@gmail.com>
2025-04-21 10:28:59 -0500

    change the color based off of correct spelling

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ def generate_words():
         words = file.read()
         return words
 
-def clear_text(event):
-    if user_type.get() == 'Type text here':
-        user_type.delete(0, tkinter.END)
+# def clear_text(event):
+#     if user_type.get() == 'Type text here':
+#         user_type.delete(0, tkinter.END)
 
 def compare_text():
     pass
@@ -37,26 +37,32 @@ text_diplay = tkinter.Text()
 text_diplay.insert(tkinter.END, generate_words())
 text_diplay.config(state='disabled')
 text_diplay.grid(columnspan=3, row=1)
-
 # user input 
 user_type = tkinter.Entry(width=40)
 user_type.grid(column=1,row=2)
-user_type.insert(index=0, string='Type text here')
-user_type.bind('<FocusIn>', clear_text)
+# user_type.insert(index=0, string='Type text here')
+# user_type.bind('<FocusIn>', clear_text)
 
 def compare():
-    print('hello')
     user_text = user_type.get()
-    text_diplay_lst = text_diplay.get('1.0', tkinter.END).strip('\n')
-    print(user_text)
+    text_diplay_lst = text_diplay.get('1.0', tkinter.END).lstrip('\n')
+    # print(user_text)
+    line = 1
+    char = 0
     for i in range(len(user_text)):
+        if text_diplay_lst[i] == '\n':
+            line += 1
+            char += 1
         if text_diplay_lst[i] == user_text[i]:
-            text_diplay.tag_add('highlight', f'')
-    window.after(2000, compare)        
-# maybe use yield so progress starts back where it ended
+            text_diplay.tag_add('highlight_green', f'{line}.{char}', f'{line}.{char + 1}')
+            text_diplay.tag_config(tagName='highlight_green', foreground='green')
+        elif text_diplay_lst[i] != user_text[i]:
+            text_diplay.tag_add('highlight_red', f'{line}.{char}', f'{line}.{char + 1}')   
+            text_diplay.tag_config(tagName='highlight_red', foreground='red') 
+        char += 1    
+    window.after(1000, compare)        
+# # maybe use yield so progress starts back where it ended
 
 compare()
 stop_watch()
-
-
 window.mainloop()
