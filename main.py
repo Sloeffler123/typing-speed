f4c5af5618ae9a1a0e7841191db66116d92bf632
Sloeffler123 <samloeffler123@gmail.com>
2025-04-23 08:41:35 -0500

    just about everything works

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import tkinter
 import random
-import sys
 TIMER = 30
+
 
 def stop_watch():
     global TIMER
+    time_window = window.after(1000, stop_watch)
     TIMER -= 1
     time_text.config(text=f'Time left: {TIMER}')
-    window.after(1000, stop_watch)
+    if TIMER <= 0:
+        window.after_cancel(time_window)
+    
 def words_per_minute(red_chars):
-
-    total = len(user_type.get()) // 5 - len(red_chars)
+    user = len(user_type.get())
+    total = ((user // 5) - len(red_chars)) // 0.5
     wpm_text.config(text=f'WPM: {total}')
     
 def generate_words():
@@ -18,13 +21,18 @@ def generate_words():
         words = file.read().splitlines()
         return random.choice(words)
 
-# def clear_text(event):
-#     if user_type.get() == 'Type text here':
-#         user_type.delete(0, tkinter.END)
-
-def compare_text():
-    pass
-
+def restart():
+    global TIMER
+    TIMER = 30
+    text_diplay.tag_remove('highlight_red', '1.0', tkinter.END)
+    text_diplay.tag_remove('highlight_green', '1.0', tkinter.END)
+    text_diplay.tag_remove('highlight_black', '1.0', tkinter.END)
+    text_diplay.tag_add('highlight_black', '1.0', tkinter.END)
+    text_diplay.tag_config(tagName='highlight_black', foreground='black')
+    user_type.delete(0, tkinter.END)
+    # window.after_cancel()
+    stop_watch()
+    compare()
 window = tkinter.Tk()
 
 wpm_text = tkinter.Label(text=f'WPM: 0')
@@ -33,9 +41,8 @@ wpm_text.grid(column=0, row=0, sticky='EW')
 time_text = tkinter.Label(text='Time left: 60')
 time_text.grid(column=1, row=0, sticky='EW')
 
-restart_button = tkinter.Button(text='Restart')
+restart_button = tkinter.Button(text='Restart', command=restart)
 restart_button.grid(column=2, row=0, sticky='EW')
-
 #text display
 text_diplay = tkinter.Text()
 text_diplay.insert(tkinter.END, generate_words())
@@ -44,19 +51,20 @@ text_diplay.grid(columnspan=3, row=1)
 # user input 
 user_type = tkinter.Entry(width=40)
 user_type.grid(column=1,row=2)
-# user_type.insert(index=0, string='Type text here')
-# user_type.bind('<FocusIn>', clear_text)
 
 def compare():
     user_text = user_type.get()
     text_diplay_lst = text_diplay.get('1.0', tkinter.END)
+    cancel_winow = window.after(100, compare) 
+    if TIMER <= 0:
+        window.after_cancel(cancel_winow)
+    if len(user_type.get()) >= len(text_diplay_lst):
+        window.after_cancel(cancel_winow)    
     line = 1
     char = 0
-
     text_diplay.tag_config(tagName='highlight_red', foreground='red')
     text_diplay.tag_config(tagName='highlight_green', foreground='green')
     text_diplay.tag_config(tagName='highlight_black', foreground='black')
-
     text_diplay.tag_remove(tagName='highlight_red', index1='1.0', index2=tkinter.END)
     text_diplay.tag_remove(tagName='highlight_green', index1='1.0', index2=tkinter.END)
     text_diplay.tag_remove(tagName='highlight_black', index1='1.0', index2=tkinter.END)
@@ -70,18 +78,10 @@ def compare():
         elif text_diplay_lst[i] != user_text[i]:  
             text_diplay.tag_add('highlight_red', f'{line}.{char}', f'{line}.{char + 1}') 
             red_char.append('i')
-        char += 1  
-    print(len(red_char))      
+        char += 1        
     text_diplay.tag_add('highlight_black', f'{line}.{len(user_text) + 1}', tkinter.END)
-    words_per_minute(red_char)  
-    window.after(100, compare)   
-        
-# # maybe use yield so progress starts back where it ended
-# text_diplay.tag_add('highlight_black', f'{line}.{len(user_text)}', tkinter.END)
+    words_per_minute(red_char)   
 
-def main():
-    compare()
-    stop_watch()
-    window.mainloop()
-
-main()
+compare()
+stop_watch()
+window.mainloop()
